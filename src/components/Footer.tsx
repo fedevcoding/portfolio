ea5bfb521cf2f7ac1d9152d58cb19d26ccbfe44a
Federico <federicocavallini100@gmail.com>
2023-12-30 13:49:25 +0100

    refactor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,26 @@
 import { discord, email, github, pipe, telegram } from "@/assets";
+import { Row } from "./ui/Row";
+import { P } from "./ui/P";
+import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="bg-primary-dark flex flex-col items-center gap-6 py-10">
-      <div className="flex items-center gap-12">
-        <img src={github.src} alt="Github" className="invert" />
-        <img src={discord.src} alt="Discord" className="invert" />
-        <img src={email.src} alt="Email" className="invert" />
-        <img src={telegram.src} alt="Telegram" />
-      </div>
-      <div className="flex items-center gap-4">
-        <p>© {new Date().getFullYear()} fedevcoding</p>
-        <img src={pipe.src} alt="pipe" className="invert" />
-        <p>fedevcoding@gmail.com</p>
-      </div>
+    <footer className="dark:bg-primary-dark bg-primary-light">
+      <Row className="py-10" direction={"col"} align={"center"} gap={"sm"}>
+        <Row align={"center"} gap={"md"}>
+          <img src={github.src} alt="Github" className="invert" />
+          <img src={discord.src} alt="Discord" className="invert" />
+          <img src={email.src} alt="Email" className="invert" />
+          <img src={telegram.src} alt="Telegram" />
+        </Row>
+        <Row align={"center"} gap={"sm"}>
+          <P>© {new Date().getFullYear()} fedevcoding</P>
+          <img src={pipe.src} alt="pipe" className="invert" />
+          <Link href={"mailto:fedevcoding@gmail.com"}>
+            <P>fedevcoding@gmail.com</P>
+          </Link>
+        </Row>
+      </Row>
     </footer>
   );
 }
