ab142e2742b6b0b05a74b56cc6a8f3eee3a875c0
Federico <federicocavallini100@gmail.com>
2023-12-31 10:36:18 +0100

    refactor project card

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -35,11 +35,15 @@ function Projects() {
         >
           <Row direction={"col"} fullHeight>
             <nav>
-              <ul>
+              <ul className="flex">
                 {PROJECTS.map((item) => (
                   <li
                     key={item.label}
-                    className={item === selectedTab ? "selected" : ""}
+                    className={
+                      item === selectedTab
+                        ? "dark:bg-gray-800 bg-neutral-400"
+                        : ""
+                    }
                     onClick={() => setSelectedTab(item)}
                   >
                     <Img
@@ -54,104 +58,106 @@ function Projects() {
                       {item.label}
                     </P>
                     {item === selectedTab ? (
-                      <motion.div className="underline" layoutId="underline" />
+                      <motion.div
+                        className="underline dark:bg-secondary-dark bg-secondary-light"
+                        layoutId="underline"
+                      />
                     ) : null}
                   </li>
                 ))}
               </ul>
             </nav>
-            <main>
-              <AnimatePresence mode="wait">
-                <motion.div
-                  key={selectedTab ? selectedTab.label : "empty"}
-                  initial={{ y: 10, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: -10, opacity: 0 }}
-                  transition={{ duration: 0.2 }}
-                >
-                  <Row direction={"col"} align={"center"} fullHeight>
-                    <Heading
-                      className="py-14"
-                      boldness={"semi-bold"}
-                      variant={"h2"}
-                    >
-                      {selectedTab.label}
-                    </Heading>
-                    <Row align={"center"} justify={"evenly"}>
-                      {selectedTab.type === "image" ? (
-                        <Link
-                          href={selectedTab.url || ""}
-                          target="_blank"
-                          className="w-[40%]"
-                        >
-                          <Img
-                            src={selectedTab.preview}
-                            alt={selectedTab.label}
-                            className="w-full rounded-lg"
-                          />
-                        </Link>
-                      ) : (
-                        <video
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={selectedTab ? selectedTab.label : "empty"}
+                initial={{ y: 10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <div>
+                  <Heading
+                    className="py-14"
+                    boldness={"semi-bold"}
+                    variant={"h2"}
+                    align={"center"}
+                  >
+                    {selectedTab.label}
+                  </Heading>
+                  <Row align={"center"} justify={"evenly"}>
+                    {selectedTab.type === "image" ? (
+                      <Link
+                        href={selectedTab.url || ""}
+                        target="_blank"
+                        className="w-[40%]"
+                      >
+                        <Img
                           src={selectedTab.preview}
-                          className="w-[40%] rounded-lg"
-                          controls
-                          muted
+                          alt={selectedTab.label}
+                          className="w-full rounded-lg"
                         />
-                      )}
-                      <Row direction={"col"} className="w-[40%]">
-                        <P size={"lg"}>
-                          <Span theme={"secondary"} boldness={"semi-bold"}>
-                            Name:
-                          </Span>
-                          &nbsp;
-                          {selectedTab.name}
-                          <br />
-                          <Span theme={"secondary"} boldness={"semi-bold"}>
-                            Description:
-                          </Span>
-                          &nbsp;
-                          {selectedTab.description}
-                          <br />
-                          <Span theme={"secondary"} boldness={"semi-bold"}>
-                            Technologies:
-                          </Span>
-                          &nbsp;
-                          {selectedTab.technologies.join(", ")}
-                          <br />
-                        </P>
-                        <Row align={"center"} gap={"sm"} className="mt-6 flex">
-                          {selectedTab.repo ? (
-                            <Link href={selectedTab.repo} target="_blank">
-                              <Img
-                                src={github.src}
-                                alt="github"
-                                size={"md"}
-                                aspect={"square"}
-                                fit={"contain"}
-                                invertDark
-                              />
-                            </Link>
-                          ) : null}
+                      </Link>
+                    ) : (
+                      <video
+                        src={selectedTab.preview}
+                        className="w-[40%] rounded-lg"
+                        controls
+                        muted
+                      />
+                    )}
+                    <Row direction={"col"} className="w-[40%]">
+                      <P size={"lg"}>
+                        <Span theme={"secondary"} boldness={"semi-bold"}>
+                          Name:
+                        </Span>
+                        &nbsp;
+                        {selectedTab.name}
+                        <br />
+                        <Span theme={"secondary"} boldness={"semi-bold"}>
+                          Description:
+                        </Span>
+                        &nbsp;
+                        {selectedTab.description}
+                        <br />
+                        <Span theme={"secondary"} boldness={"semi-bold"}>
+                          Technologies:
+                        </Span>
+                        &nbsp;
+                        {selectedTab.technologies.join(", ")}
+                        <br />
+                      </P>
+                      <Row align={"center"} gap={"sm"} className="mt-6 flex">
+                        {selectedTab.repo ? (
+                          <Link href={selectedTab.repo} target="_blank">
+                            <Img
+                              src={github.src}
+                              alt="github"
+                              size={"md"}
+                              aspect={"square"}
+                              fit={"contain"}
+                              invertDark
+                            />
+                          </Link>
+                        ) : null}
 
-                          {selectedTab.url ? (
-                            <Link href={selectedTab.url} target="_blank">
-                              <Img
-                                src={url.src}
-                                alt="link"
-                                size={"sm-2"}
-                                aspect={"square"}
-                                fit={"contain"}
-                                invertLight
-                              />
-                            </Link>
-                          ) : null}
-                        </Row>
+                        {selectedTab.url ? (
+                          <Link href={selectedTab.url} target="_blank">
+                            <Img
+                              src={url.src}
+                              alt="link"
+                              size={"sm-2"}
+                              aspect={"square"}
+                              fit={"contain"}
+                              invertLight
+                            />
+                          </Link>
+                        ) : null}
                       </Row>
                     </Row>
                   </Row>
-                </motion.div>
-              </AnimatePresence>
-            </main>
+                </div>
+              </motion.div>
+            </AnimatePresence>
           </Row>
         </Section>
       </Section>
