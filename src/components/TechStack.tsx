83231013cc91d3039af128bde82f08846539401b
Federico <federicocavallini100@gmail.com>
2024-01-06 07:21:05 +0100

    add some other animations

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -3,10 +3,12 @@ import Card from "./Card";
 import { techStack } from "@/data/techStack";
 import { P } from "./ui/P";
 import { Heading } from "./ui/Heading";
-import { Row } from "./ui/Row";
+import { rowVariants } from "./ui/Row";
 import Line from "./ui/Line";
 import Section from "./ui/Section";
 import { useBreakPoint } from "react-use-breakpoint";
+import { motion } from "framer-motion";
+import { cn } from "@/lib";
 
 function TechStack() {
   const { max } = useBreakPoint();
@@ -15,25 +17,47 @@ function TechStack() {
     <>
       <Section className="pb-32" id="tech-stack" theme="secondary">
         <div className="py-24">
-          <P theme={"secondary"} align={"center"}>
-            Some of my
-          </P>
-          <Heading letterGap={"xl"} align={"center"}>
-            TECH STACK
-          </Heading>
+          <motion.div
+            initial={{ opacity: 0, translateY: -20 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5 }}
+          >
+            <P theme={"secondary"} align={"center"}>
+              Some of my
+            </P>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, translateY: -20 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5 }}
+          >
+            <Heading letterGap={"xl"} align={"center"}>
+              TECH STACK
+            </Heading>
+          </motion.div>
         </div>
 
-        <Row
-          justify={"center"}
-          align={"center"}
-          gap={max.isXl ? "xl" : "3xl"}
-          wrap={max.isMd ? "wrap" : "none"}
-          className="px-20"
+        <motion.div
+          initial={{ opacity: 0, translateY: 20 }}
+          whileInView={{ opacity: 1, translateY: 0 }}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
+          className={cn(
+            rowVariants({
+              justify: "center",
+              align: "center",
+              gap: max.isXl ? "xl" : "3xl",
+              wrap: max.isMd ? "wrap" : "none",
+            }),
+            "px-20"
+          )}
         >
           <Card name="Front End" mainIcon={html} icons={techStack.frontEnd} />
           <Card name="Back End" mainIcon={code} icons={techStack.backEnd} />
           <Card name="Other" mainIcon={tools} icons={techStack.other} />
-        </Row>
+        </motion.div>
       </Section>
       <Line />
     </>
