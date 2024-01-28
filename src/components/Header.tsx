37c1bd332f7a98f80e1c6913a33631e0aaf81f83
fedevcoding <fedevcoding@gmail.com>
2024-01-28 19:11:13 +0100

    add more animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,115 +9,123 @@ import Drawer from "./Drawer";
 import { useCycle } from "framer-motion";
 
 function Header() {
-  const { max } = useBreakPoint();
-  const [isOpen, toggle] = useCycle(false, true);
-  const { isDark, toggleTheme } = useThemeContext();
+ const { max } = useBreakPoint();
+ const [isOpen, toggle] = useCycle(false, true);
+ const { isDark, toggleTheme } = useThemeContext();
 
-  return (
-    <header className="h-header relative">
-      {/* Drawer component for mobiles */}
-      <Drawer open={max.isMd && isOpen} toggle={toggle} />
+ return (
+  <header className="h-header relative">
+   {/* Drawer component for mobiles */}
+   <Drawer open={max.isMd && isOpen} toggle={toggle} />
 
-      <Row
-        align={"center"}
-        justify={"center"}
-        gap={max.isLg ? "2xl" : "5xl"}
-        fullHeight
-      >
-        <a href="/">
-          <Img alt="logo" src={logo} size={"lg"} />
-        </a>
-        {max.isMd ? (
-          <Row gap={"md"} align={"center"}>
-            <Img
-              src={isDark ? moon : sun}
-              alt="theme"
-              onClick={toggleTheme}
-              aspect={"square"}
-              fit="contain"
-              size={"sm-2"}
-              className="cursor-pointer"
-            />
-            <Burger isOpen={isOpen} toggle={toggle} />
-          </Row>
-        ) : (
-          <>
-            <nav>
-              <ul>
-                <Row gap={"md"}>
-                  <li>
-                    <a href="#home">Home</a>
-                  </li>
-                  <li>
-                    <a href="#about">About</a>
-                  </li>
-                  <li>
-                    <a href={"#tech-stack"}>Tech Stack</a>
-                  </li>
-                  <li>
-                    <a href={"#projects"}>Projects</a>
-                  </li>
-                </Row>
-              </ul>
-            </nav>
-            <Row align={"center"} gap={"sm"}>
-              <Img
-                src={isDark ? moon : sun}
-                alt="theme"
-                onClick={toggleTheme}
-                aspect={"square"}
-                fit="contain"
-                size={"sm-2"}
-                className="cursor-pointer"
-              />
-              <Img
-                src={pipe}
-                alt="pipe"
-                className="dark:invert"
-                aspect={"square"}
-                fit="contain"
-                size={"sm-2"}
-              />
-              <a href={"https://github.com/fedevcoding"} target="_blank">
-                <Img
-                  src={github}
-                  alt="github"
-                  className="dark:invert"
-                  aspect={"square"}
-                  fit="contain"
-                  size={"sm-2"}
-                />
-              </a>
-              <a
-                href={"https://discordapp.com/users/646076973025722388"}
-                target="_blank"
-              >
-                <Img
-                  src={discord}
-                  alt="discord"
-                  className="dark:invert"
-                  aspect={"square"}
-                  fit="contain"
-                  size={"sm-2"}
-                />
-              </a>
+   <Row
+    align={"center"}
+    justify={"center"}
+    gap={max.isLg ? "2xl" : "5xl"}
+    fullHeight
+   >
+    <a href="/">
+     <Img alt="logo" src={logo} size={"lg"} />
+    </a>
+    {max.isMd ? (
+     <Row gap={"md"} align={"center"}>
+      <Img
+       src={isDark ? moon : sun}
+       alt="theme"
+       onClick={toggleTheme}
+       aspect={"square"}
+       fit="contain"
+       size={"sm-2"}
+       className="cursor-pointer"
+      />
+      <Burger isOpen={isOpen} toggle={toggle} />
+     </Row>
+    ) : (
+     <>
+      <nav>
+       <ul>
+        <Row gap={"md"}>
+         <li>
+          <a href="#home" data-dark={isDark}>
+           Home
+          </a>
+         </li>
+         <li>
+          <a href="#about" data-dark={isDark}>
+           About
+          </a>
+         </li>
+         <li>
+          <a href={"#tech-stack"} data-dark={isDark}>
+           Tech Stack
+          </a>
+         </li>
+         <li>
+          <a href={"#projects"} data-dark={isDark}>
+           Projects
+          </a>
+         </li>
+        </Row>
+       </ul>
+      </nav>
+      <Row align={"center"} gap={"sm"}>
+       <Img
+        src={isDark ? moon : sun}
+        alt="theme"
+        onClick={toggleTheme}
+        aspect={"square"}
+        fit="contain"
+        size={"sm-2"}
+        className="cursor-pointer"
+       />
+       <Img
+        src={pipe}
+        alt="pipe"
+        className="dark:invert"
+        aspect={"square"}
+        fit="contain"
+        size={"sm-2"}
+       />
+       <a href={"https://github.com/fedevcoding"} target="_blank">
+        <Img
+         src={github}
+         alt="github"
+         className="dark:invert"
+         aspect={"square"}
+         fit="contain"
+         size={"sm-2"}
+        />
+       </a>
+       <a
+        href={"https://discordapp.com/users/646076973025722388"}
+        target="_blank"
+       >
+        <Img
+         src={discord}
+         alt="discord"
+         className="dark:invert"
+         aspect={"square"}
+         fit="contain"
+         size={"sm-2"}
+        />
+       </a>
 
-              <a href={"mailto:fedevcoding@gmail.com"} target="_blank">
-                <Img
-                  src={email}
-                  alt="email"
-                  className="dark:invert"
-                  aspect={"square"}
-                  fit="contain"
-                  size={"sm-2"}
-                />
-              </a>
-            </Row>
-          </>
-        )}
+       <a href={"mailto:fedevcoding@gmail.com"} target="_blank">
+        <Img
+         src={email}
+         alt="email"
+         className="dark:invert"
+         aspect={"square"}
+         fit="contain"
+         size={"sm-2"}
+        />
+       </a>
       </Row>
-    </header>
-  );
+     </>
+    )}
+   </Row>
+  </header>
+ );
 }
 
 export default Header;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,90 +11,86 @@ import { Img } from "./ui/Img";
 import { useBreakPoint } from "react-use-breakpoint";
 
 function Home() {
-  const { isDark } = useThemeContext();
-  const { max } = useBreakPoint();
+ const { isDark } = useThemeContext();
+ const { max } = useBreakPoint();
 
-  useEffect(() => {
-    if (!max.isMd) {
-      document.addEventListener("mousemove", animateEyes);
-    }
+ useEffect(() => {
+  if (!max.isMd) {
+   document.addEventListener("mousemove", animateEyes);
+  }
 
-    return () => {
-      if (!max.isMd) {
-        document.removeEventListener("mousemove", animateEyes);
-      }
-    };
-  }, [max]);
+  return () => {
+   if (!max.isMd) {
+    document.removeEventListener("mousemove", animateEyes);
+   }
+  };
+ }, [max]);
 
-  return (
-    <>
-      <Section
-        className={`${max.isSm ? "" : "h-screen"}`}
-        id="home"
-        theme="primary"
+ return (
+  <>
+   <Section
+    className={`${max.isSm ? "" : "h-screen"}`}
+    id="home"
+    theme="primary"
+   >
+    <Header />
+    <Row
+     justify={"center"}
+     align={"center"}
+     className={`${max.isSm ? "pb-20" : "h-home"} px-20`}
+     gap={max.isMd ? "xl" : "2xl"}
+     wrap={max.isSm ? "wrap" : "none"}
+    >
+     <Row
+      direction={"col"}
+      fullHeight
+      justify={"center"}
+      gap={"md"}
+      align={max.isSm ? "center" : "start"}
+     >
+      <Heading
+       letterGap={"2xl"}
+       variant={"h1"}
+       align={max.isSm ? "center" : "left"}
       >
-        <Header />
-        <Row
-          justify={"center"}
-          align={"center"}
-          className={`${max.isSm ? "pb-20" : "h-home"} px-20`}
-          gap={max.isMd ? "xl" : "2xl"}
-          wrap={max.isSm ? "wrap" : "none"}
-        >
-          <Row
-            direction={"col"}
-            fullHeight
-            justify={"center"}
-            gap={"md"}
-            align={max.isSm ? "center" : "start"}
-          >
-            <Heading
-              letterGap={"2xl"}
-              variant={"h1"}
-              align={max.isSm ? "center" : "left"}
-            >
-              FEDERICO
-              <br />
-              CAVALLINI
-            </Heading>
-            <P size={"lg"} align={max.isSm ? "center" : "left"}>
-              Hi, I&apos;m Fedev
-              <br />A young full-stack developer from Italy
-            </P>
+       FEDERICO
+       <br />
+       CAVALLINI
+      </Heading>
+      <P size={"lg"} align={max.isSm ? "center" : "left"}>
+       Hi, I&apos;m Fedev
+       <br />A young full-stack developer from Italy
+      </P>
 
-            <a href={"#about"}>
-              <button className="dark:bg-secondary-dark bg-secondary-light text-2xl text-black py-4 px-12 rounded-lg">
-                About Me
-              </button>
-            </a>
-          </Row>
+      <a href={"#about"}>
+       <button className="dark:bg-secondary-dark bg-secondary-light text-2xl text-black py-4 px-12 rounded-lg about-me">
+        About Me
+       </button>
+      </a>
+     </Row>
 
-          <div className="relative">
-            <Img
-              src={isDark ? homePfpDark : homePfpLight}
-              alt="Pfp"
-              size={"3xl"}
-            />
-            <div className="leftEye eyes"></div>
-            <div className="rightEye eyes"></div>
-          </div>
-        </Row>
-      </Section>
-    </>
-  );
+     <div className="relative">
+      <Img src={isDark ? homePfpDark : homePfpLight} alt="Pfp" size={"3xl"} />
+      <div className="leftEye eyes"></div>
+      <div className="rightEye eyes"></div>
+     </div>
+    </Row>
+   </Section>
+  </>
+ );
 }
 
 export default Home;
 
 function animateEyes(e: MouseEvent) {
-  const eyes = document.querySelectorAll(".eyes");
-  eyes.forEach((eye) => {
-    const x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
-    const y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
-    const radian = Math.atan2(e.pageX - x, e.pageY - y);
-    const rotate = radian * (180 / Math.PI) * -1 + 270;
-    if (eye instanceof HTMLElement) {
-      eye.style.transform = `rotate(${rotate}deg)`;
-    }
-  });
+ const eyes = document.querySelectorAll(".eyes");
+ eyes.forEach((eye) => {
+  const x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+  const y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+  const radian = Math.atan2(e.pageX - x, e.pageY - y);
+  const rotate = radian * (180 / Math.PI) * -1 + 270;
+  if (eye instanceof HTMLElement) {
+   eye.style.transform = `rotate(${rotate}deg)`;
+  }
+ });
 }
