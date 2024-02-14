e5bd3bf1c0a9e6679f1bd9fdb470757a17bcd52b
fedevcoding <fedevcoding@gmail.com>
2024-02-14 18:16:16 +0100

    ⭐️ Image optimization

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,63 +10,58 @@ import { Img } from "./ui/Img";
 import { useBreakPoint } from "react-use-breakpoint";
 
 function About() {
-  const { isDark } = useThemeContext();
-  const { max } = useBreakPoint();
+ const { isDark } = useThemeContext();
+ const { max } = useBreakPoint();
 
-  return (
-    <>
-      <Section className="pb-32" id="about" theme="secondary">
-        <Img
-          src={isDark ? waveDark : waveLight}
-          className="w-screen"
-          alt="wave"
-        />
-        <Row
-          justify={"center"}
-          align={"center"}
-          gap={"3xl"}
-          wrap={max.isMd ? "wrap" : "none"}
-          className={`${max.isSm ? " px-10" : "px-40"}`}
-        >
-          <motion.div
-            initial={{ x: -50, opacity: 0.5 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, delay: 0.2 }}
-            className={`${max.isMd ? "order-2" : "order-none"}`}
-          >
-            <Img size={"3xl"} src={isDark ? meDark : meLight} alt="Me" />
-          </motion.div>
-          <motion.div
-            initial={{ x: 50, opacity: 0.5 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, delay: 0.2 }}
-          >
-            <Row
-              direction="col"
-              className={`${max.isMd ? "max-w-[60vw]" : "max-w-[25vw]"}`}
-              align={max.isMd ? "center" : "start"}
-            >
-              <P theme={"secondary"}>A little bit</P>
-              <Heading letterGap={"xl"}>ABOUT ME</Heading>
-              <P theme={"secondary"}>Front-end/back-end developer</P>
-              <br />
-              <P size={"xl"} align={max.isMd ? "center" : "left"}>
-                My name is Federico Cavallini, born in the uk and raised in
-                Florence, Italy. I’m a very young developer that has been coding
-                since early 2021 and very confident with both front-end and
-                back-end.
-                <br />
-                My other hobbies are mountain biking and listening to music.
-              </P>
-            </Row>
-          </motion.div>
-        </Row>
-      </Section>
-      <Line />
-    </>
-  );
+ return (
+  <>
+   <Section className="pb-32" id="about" theme="secondary">
+    <Img src={isDark ? waveDark : waveLight} className="w-screen" alt="wave" />
+    <Row
+     justify={"center"}
+     align={"center"}
+     gap={"3xl"}
+     wrap={max.isMd ? "wrap" : "none"}
+     className={`${max.isSm ? " px-10" : "px-40"}`}
+    >
+     <motion.div
+      initial={{ x: -50, opacity: 0.5 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, delay: 0.2 }}
+      className={`${max.isMd ? "order-2" : "order-none"}`}
+     >
+      <Img size={"3xl"} src={isDark ? meDark : meLight} alt="Me" />
+     </motion.div>
+     <motion.div
+      initial={{ x: 50, opacity: 0.5 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, delay: 0.2 }}
+     >
+      <Row
+       direction="col"
+       className={`${max.isMd ? "max-w-[60vw]" : "max-w-[25vw]"}`}
+       align={max.isMd ? "center" : "start"}
+      >
+       <P theme={"secondary"}>A little bit</P>
+       <Heading letterGap={"xl"}>ABOUT ME</Heading>
+       <P theme={"secondary"}>Front-end/back-end developer</P>
+       <br />
+       <P size={"xl"} align={max.isMd ? "center" : "left"}>
+        My name is Federico Cavallini, born in the uk and raised in Florence,
+        Italy. I’m a very young developer that has been coding since early 2021
+        and very confident with both front-end and back-end.
+        <br />
+        My other hobbies are mountain biking and listening to music.
+       </P>
+      </Row>
+     </motion.div>
+    </Row>
+   </Section>
+   <Line />
+  </>
+ );
 }
 
 export default About;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,191 +15,187 @@ import { Img } from "./ui/Img";
 import { useBreakPoint } from "react-use-breakpoint";
 
 function Projects() {
-  const { max } = useBreakPoint();
-  const [selectedTab, setSelectedTab] = useState(PROJECTS[0]);
+ const { max } = useBreakPoint();
+ const [selectedTab, setSelectedTab] = useState(PROJECTS[0]);
 
-  return (
-    <>
-      <Section className="pb-20" id="projects" theme="secondary">
-        <div className="py-24">
-          <motion.div
-            initial={{ opacity: 0, translateY: -20 }}
-            whileInView={{ opacity: 1, translateY: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.5 }}
-          >
-            <P theme={"secondary"} align={"center"}>
-              Some of my
-            </P>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, translateY: -20 }}
-            whileInView={{ opacity: 1, translateY: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.5 }}
-          >
-            <Heading letterGap={"xl"} align={"center"}>
-              PROJECTS
-            </Heading>
-          </motion.div>
-        </div>
+ return (
+  <>
+   <Section className="pb-20" id="projects" theme="secondary">
+    <div className="py-24">
+     <motion.div
+      initial={{ opacity: 0, translateY: -20 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+     >
+      <P theme={"secondary"} align={"center"}>
+       Some of my
+      </P>
+     </motion.div>
+     <motion.div
+      initial={{ opacity: 0, translateY: -20 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+     >
+      <Heading letterGap={"xl"} align={"center"}>
+       PROJECTS
+      </Heading>
+     </motion.div>
+    </div>
 
+    <motion.div
+     initial={{ opacity: 0, translateY: 20 }}
+     whileInView={{ opacity: 1, translateY: 0 }}
+     transition={{ duration: 0.5 }}
+     viewport={{ once: true }}
+    >
+     <Section
+      theme="primary"
+      className="w-[70vw] rounded-lg mx-auto projects-box pb-10"
+     >
+      <Row direction={"col"} fullHeight>
+       <nav>
+        <ul className="flex flex-wrap">
+         {PROJECTS.map((item) => (
+          <li
+           key={item.label}
+           className={
+            item === selectedTab ? "dark:bg-gray-800 bg-neutral-400" : ""
+           }
+           onClick={() => setSelectedTab(item)}
+          >
+           <Img
+            invertLight
+            src={item.icon}
+            alt={item.label}
+            size={"sm"}
+            aspect={"square"}
+            fit={"contain"}
+           />
+           <P boldness={"semi-bold"} size={"sm"}>
+            {item.label}
+           </P>
+           {item === selectedTab ? (
+            <motion.div
+             className="underline dark:bg-secondary-dark bg-secondary-light"
+             layoutId="underline"
+            />
+           ) : null}
+          </li>
+         ))}
+        </ul>
+       </nav>
+       <AnimatePresence mode="wait">
         <motion.div
-          initial={{ opacity: 0, translateY: 20 }}
-          whileInView={{ opacity: 1, translateY: 0 }}
-          transition={{ duration: 0.5 }}
-          viewport={{ once: true }}
+         key={selectedTab ? selectedTab.label : "empty"}
+         initial={{ y: 10, opacity: 0 }}
+         animate={{ y: 0, opacity: 1 }}
+         exit={{ y: -10, opacity: 0 }}
+         transition={{ duration: 0.2 }}
         >
-          <Section
-            theme="primary"
-            className="w-[70vw] rounded-lg mx-auto projects-box pb-10"
+         <div>
+          <Heading
+           className="py-14"
+           boldness={"semi-bold"}
+           variant={"h2"}
+           align={"center"}
           >
-            <Row direction={"col"} fullHeight>
-              <nav>
-                <ul className="flex flex-wrap">
-                  {PROJECTS.map((item) => (
-                    <li
-                      key={item.label}
-                      className={
-                        item === selectedTab
-                          ? "dark:bg-gray-800 bg-neutral-400"
-                          : ""
-                      }
-                      onClick={() => setSelectedTab(item)}
-                    >
-                      <Img
-                        invertLight
-                        src={item.icon}
-                        alt={item.label}
-                        size={"sm"}
-                        aspect={"square"}
-                        fit={"contain"}
-                      />
-                      <P boldness={"semi-bold"} size={"sm"}>
-                        {item.label}
-                      </P>
-                      {item === selectedTab ? (
-                        <motion.div
-                          className="underline dark:bg-secondary-dark bg-secondary-light"
-                          layoutId="underline"
-                        />
-                      ) : null}
-                    </li>
-                  ))}
-                </ul>
-              </nav>
-              <AnimatePresence mode="wait">
-                <motion.div
-                  key={selectedTab ? selectedTab.label : "empty"}
-                  initial={{ y: 10, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: -10, opacity: 0 }}
-                  transition={{ duration: 0.2 }}
-                >
-                  <div>
-                    <Heading
-                      className="py-14"
-                      boldness={"semi-bold"}
-                      variant={"h2"}
-                      align={"center"}
-                    >
-                      {selectedTab.label}
-                    </Heading>
-                    <Row
-                      align={"center"}
-                      justify={"evenly"}
-                      direction={max.isSm ? "col" : "row"}
-                      gap={max.isSm ? "md" : "none"}
-                    >
-                      {selectedTab.type === "image" ? (
-                        <a
-                          href={selectedTab.url || ""}
-                          target="_blank"
-                          className={`${
-                            max.isSm ? "order-2 w-[80%]" : "w-[40%]"
-                          } `}
-                        >
-                          <Img
-                            src={selectedTab.preview}
-                            alt={selectedTab.label}
-                            className={`w-full rounded-lg`}
-                            aspect={"video"}
-                            fit={"contain"}
-                          />
-                        </a>
-                      ) : (
-                        <video
-                          src={selectedTab.preview}
-                          className={`${
-                            max.isSm ? "order-2 w-[80%]" : "w-[40%]"
-                          } rounded-lg`}
-                          controls
-                          muted
-                        />
-                      )}
-                      <Row
-                        direction={"col"}
-                        className={`${max.isSm ? "w-[80%]" : "w-[40%]"}`}
-                      >
-                        <P size={"lg"}>
-                          <Span theme={"secondary"} boldness={"semi-bold"}>
-                            Name:
-                          </Span>
-                          &nbsp;
-                          {selectedTab.name}
-                          <br />
-                          <Span theme={"secondary"} boldness={"semi-bold"}>
-                            Description:
-                          </Span>
-                          &nbsp;
-                          {selectedTab.description}
-                          <br />
-                          <Span theme={"secondary"} boldness={"semi-bold"}>
-                            Technologies:
-                          </Span>
-                          &nbsp;
-                          {selectedTab.technologies.join(", ")}
-                          <br />
-                        </P>
-                        <Row align={"center"} gap={"sm"} className="mt-6 flex">
-                          {selectedTab.repo ? (
-                            <a href={selectedTab.repo} target="_blank">
-                              <Img
-                                src={github}
-                                alt="github"
-                                size={"md"}
-                                aspect={"square"}
-                                fit={"contain"}
-                                invertDark
-                              />
-                            </a>
-                          ) : null}
+           {selectedTab.label}
+          </Heading>
+          <Row
+           align={"center"}
+           justify={"evenly"}
+           direction={max.isSm ? "col" : "row"}
+           gap={max.isSm ? "md" : "none"}
+          >
+           {selectedTab.type === "image" ? (
+            <a
+             href={selectedTab.url || ""}
+             target="_blank"
+             className={`${max.isSm ? "order-2 w-[80%]" : "w-[40%]"} `}
+            >
+             <Img
+              src={selectedTab.preview}
+              alt={selectedTab.label}
+              className={`w-full rounded-lg`}
+              aspect={"video"}
+              fit={"contain"}
+             />
+            </a>
+           ) : (
+            <video
+             src={selectedTab.preview}
+             className={`${
+              max.isSm ? "order-2 w-[80%]" : "w-[40%]"
+             } rounded-lg h-[300px]`}
+             controls
+             muted
+            />
+           )}
+           <Row
+            direction={"col"}
+            className={`${max.isSm ? "w-[80%]" : "w-[40%]"}`}
+           >
+            <P size={"lg"}>
+             <Span theme={"secondary"} boldness={"semi-bold"}>
+              Name:
+             </Span>
+             &nbsp;
+             {selectedTab.name}
+             <br />
+             <Span theme={"secondary"} boldness={"semi-bold"}>
+              Description:
+             </Span>
+             &nbsp;
+             {selectedTab.description}
+             <br />
+             <Span theme={"secondary"} boldness={"semi-bold"}>
+              Technologies:
+             </Span>
+             &nbsp;
+             {selectedTab.technologies.join(", ")}
+             <br />
+            </P>
+            <Row align={"center"} gap={"sm"} className="mt-6 flex">
+             {selectedTab.repo ? (
+              <a href={selectedTab.repo} target="_blank">
+               <Img
+                src={github}
+                alt="github"
+                size={"md"}
+                aspect={"square"}
+                fit={"contain"}
+                invertDark
+               />
+              </a>
+             ) : null}
 
-                          {selectedTab.url ? (
-                            <a href={selectedTab.url} target="_blank">
-                              <Img
-                                src={url}
-                                alt="link"
-                                size={"sm-2"}
-                                aspect={"square"}
-                                fit={"contain"}
-                                invertLight
-                              />
-                            </a>
-                          ) : null}
-                        </Row>
-                      </Row>
-                    </Row>
-                  </div>
-                </motion.div>
-              </AnimatePresence>
+             {selectedTab.url ? (
+              <a href={selectedTab.url} target="_blank">
+               <Img
+                src={url}
+                alt="link"
+                size={"sm-2"}
+                aspect={"square"}
+                fit={"contain"}
+                invertLight
+               />
+              </a>
+             ) : null}
             </Row>
-          </Section>
+           </Row>
+          </Row>
+         </div>
         </motion.div>
-      </Section>
-      <Line />
-    </>
-  );
+       </AnimatePresence>
+      </Row>
+     </Section>
+    </motion.div>
+   </Section>
+   <Line />
+  </>
+ );
 }
 
 export default Projects;
