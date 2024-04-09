882f3f37e734143c4f3369a499d2d7cc91a5a484
fedevcoding <fedevcoding@gmail.com>
2024-04-09 18:21:06 +0200

    update tech stack

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,57 +4,57 @@ import { Row } from "./ui/Row";
 import Section from "./ui/Section";
 
 type Props = {
+ name: string;
+ mainIcon: string;
+ icons: {
   name: string;
-  mainIcon: string;
-  icons: {
-    name: string;
-    icon: string;
-  }[];
+  icon: string;
+ }[];
 };
 
 function Card({ name, mainIcon, icons }: Props) {
-  return (
-    <Section
-      theme="primary"
-      className="w-80 h-96 rounded-lg"
-      style={{ boxShadow: "4px 9px 24.4px 10px rgba(0, 0, 0, 0.25)" }}
-    >
-      <Row direction={"col"} fullHeight>
-        <div className="rounded-t-lg dark:bg-secondary-dark bg-secondary-light py-4">
-          <Img
-            src={mainIcon}
-            alt={name}
-            className="mx-auto"
-            size={"md"}
-            aspect={"square"}
-            fit={"contain"}
-          />
-          <P
-            boldness={"semi-bold"}
-            align={"center"}
-            theme={"opposite"}
-            className="text-black"
-          >
-            {name}
-          </P>
-        </div>
-        <div className="grid grid-cols-2 flex-1">
-          {icons.map((icon, i) => (
-            <Row className="pl-6" align={"center"} gap={"xs"} key={i}>
-              <Img
-                src={icon.icon}
-                alt={icon.name}
-                aspect={"square"}
-                fit={"contain"}
-                size={"sm-2"}
-              />
-              <P>{icon.name}</P>
-            </Row>
-          ))}
-        </div>
+ return (
+  <Section
+   theme="primary"
+   className="w-80 h-[28rem] rounded-lg"
+   style={{ boxShadow: "4px 9px 24.4px 10px rgba(0, 0, 0, 0.25)" }}
+  >
+   <Row direction={"col"} fullHeight>
+    <div className="rounded-t-lg dark:bg-secondary-dark bg-secondary-light py-4">
+     <Img
+      src={mainIcon}
+      alt={name}
+      className="mx-auto"
+      size={"md"}
+      aspect={"square"}
+      fit={"contain"}
+     />
+     <P
+      boldness={"semi-bold"}
+      align={"center"}
+      theme={"opposite"}
+      className="text-black"
+     >
+      {name}
+     </P>
+    </div>
+    <div className="grid grid-cols-2 flex-1">
+     {icons.map((icon, i) => (
+      <Row className="pl-6" align={"center"} gap={"xs"} key={i}>
+       <Img
+        src={icon.icon}
+        alt={icon.name}
+        aspect={"square"}
+        fit={"contain"}
+        size={"sm-2"}
+       />
+       <P>{icon.name}</P>
       </Row>
-    </Section>
-  );
+     ))}
+    </div>
+   </Row>
+  </Section>
+ );
 }
 
 export default Card;
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -11,57 +11,60 @@ import { motion } from "framer-motion";
 import { cn } from "@/lib";
 
 function TechStack() {
-  const { max } = useBreakPoint();
+ const { max } = useBreakPoint();
 
-  return (
-    <>
-      <Section className="pb-32" id="tech-stack" theme="secondary">
-        <div className="py-24">
-          <motion.div
-            initial={{ opacity: 0, translateY: -20 }}
-            whileInView={{ opacity: 1, translateY: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.5 }}
-          >
-            <P theme={"secondary"} align={"center"}>
-              Some of my
-            </P>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, translateY: -20 }}
-            whileInView={{ opacity: 1, translateY: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.5 }}
-          >
-            <Heading letterGap={"xl"} align={"center"}>
-              TECH STACK
-            </Heading>
-          </motion.div>
-        </div>
+ return (
+  <>
+   <Section className="pb-20" id="tech-stack" theme="secondary">
+    <div className="py-24">
+     <motion.div
+      initial={{ opacity: 0, translateY: -20 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+     >
+      <P theme={"secondary"} align={"center"}>
+       Some of my
+      </P>
+     </motion.div>
+     <motion.div
+      initial={{ opacity: 0, translateY: -20 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+     >
+      <Heading letterGap={"xl"} align={"center"}>
+       TECH STACK
+      </Heading>
+     </motion.div>
+    </div>
 
-        <motion.div
-          initial={{ opacity: 0, translateY: 20 }}
-          whileInView={{ opacity: 1, translateY: 0 }}
-          transition={{ duration: 0.5 }}
-          viewport={{ once: true }}
-          className={cn(
-            rowVariants({
-              justify: "center",
-              align: "center",
-              gap: max.isXl ? "xl" : "3xl",
-              wrap: max.isMd ? "wrap" : "none",
-            }),
-            "px-20"
-          )}
-        >
-          <Card name="Front End" mainIcon={html} icons={techStack.frontEnd} />
-          <Card name="Back End" mainIcon={code} icons={techStack.backEnd} />
-          <Card name="Other" mainIcon={tools} icons={techStack.other} />
-        </motion.div>
-      </Section>
-      <Line />
-    </>
-  );
+    <motion.div
+     initial={{ opacity: 0, translateY: 20 }}
+     whileInView={{ opacity: 1, translateY: 0 }}
+     transition={{ duration: 0.5 }}
+     viewport={{ once: true }}
+     className={cn(
+      rowVariants({
+       justify: "center",
+       align: "center",
+       gap: max.isXl ? "xl" : "3xl",
+       wrap: max.isMd ? "wrap" : "none",
+      }),
+      "px-20"
+     )}
+    >
+     <Card name="Front End" mainIcon={html} icons={techStack.frontEnd} />
+     <Card name="Back End" mainIcon={code} icons={techStack.backEnd} />
+     <Card name="Other" mainIcon={tools} icons={techStack.other} />
+    </motion.div>
+    <P boldness={"semi-bold"} align={"center"} className="mt-12">
+     And other...
+    </P>
+   </Section>
+   <Line />
+  </>
+ );
 }
 
 export default TechStack;
